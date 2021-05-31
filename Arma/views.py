c69a9e2edad3c80ec504b2712414d045a586a3d3
Tomas1207 <tomasfernandes98@gmail.com>
2021-05-31 15:30:14 +0100

    ExtraData For status in frontend

--- Arma/views.py
+++ Arma/views.py
@@ -9,7 +9,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import *
 from rest_framework.pagination import LimitOffsetPagination
-from .models import medic
+from .models import *
 from .serializers import *
 from users.models import NewUser
 from users.serializers import RegisterNewUser
@@ -22,27 +22,36 @@ from rest_framework_simplejwt.authentication import JWTAuthentication as jwt
 class MedicStatus(APIView,LimitOffsetPagination):
     permission_classes = [IsAuthenticated]
     def get(self,request,format='json'):
-        return Response(endpoints.NormalEndPoint(self,medic,request,medicSerializer,{'healer_id':request.user.steamID,'mission':request.GET.get('mission')}),status=status.HTTP_200_OK)
+        kwargs={
+            "mostusedItem":Extradata(self,medic,'typeOfHeal','healer_id'),
+            "bestMedic": Extradata(self,medic,'healer_id','healer_id'),
+            "holey": Extradata(self,medic,'healed_id','healed_id'),
+            "mostShootZone": Extradata(self,medic,'hitLocation','healed_id')
+        }
+        return Response(endpoints.NormalEndPoint(self,medic,request,medicSerializer,{'mission':request.GET.get('mission')},**kwargs),status=status.HTTP_200_OK)
 
 
 class unconsciousStatus(APIView,LimitOffsetPagination):
     permission_classes = [IsAuthenticated]
     def get(self,request,format='json'):
         
-        serialiedData = pagintes.paginatefunc(self,unconscious,request,unconsciousSerializer,{'unit_id':request.user.steamID})
-        kwargs ={"DeadCount" : triggerHappy(self,serialiedData.data)}
-        return Response(endpoints.NormalEndPoint(self,unconscious,request,unconsciousSerializer,{'unit_id':request.user.id},**kwargs),status=status.HTTP_200_OK)
+        serialiedData = pagintes.paginatefunc(self,unconscious,request,unconsciousSerializer,{'mission':request.GET.get('mission')})
+        kwargs ={
+            "DeadCount" : DeadCounts(self,serialiedData.data),
+            "TheGroundHugger": Extradata(self,unconscious,'unit_id','unit_id')
+        }
+        return Response(endpoints.NormalEndPoint(self,unconscious,request,unconsciousSerializer,{'mission':request.GET.get('mission')},**kwargs),status=status.HTTP_200_OK)
     
-
 class shootsFiredStatus(APIView,LimitOffsetPagination):
     permission_classes = [IsAuthenticated]
 
     def get(self,request,format='json'):
-        print(request.GET.get('mission'))
-         
-        serialiedData = pagintes.paginatefunc(self,shootsfired,request,shootsFiredSerializer,{'unit_id':request.user.steamID})
-        kwargs ={"triggerHappy" : triggerHappy(self,shootsfired,'unit')}
-        print(kwargs["triggerHappy"])
+        kwargs ={
+            "TriggerHappy" : Extradata(self,shootsfired,'unit','unit'),
+            "mostWeaponUsed":Extradata(self,shootsfired,'weapon','unit'),
+            "mostUsedMode":Extradata(self,shootsfired,'mode','unit'),
+            "AmmoDispenser":Extradata(self,shootsfired,'ammo','unit')
+            }
         return Response(endpoints.NormalEndPoint(self,shootsfired,request,shootsFiredSerializer,{'mission':request.GET.get('mission')},**kwargs),status=status.HTTP_200_OK)
 
         
@@ -63,21 +72,16 @@ def DeadCounts(self,data):
         
         i += 1
     
-    if data[-1]["state"]:
-        deadsCounts  += 1
+        if data[-1]["state"]:
+            deadsCounts  += 1
 
     return deadsCounts
 
-def triggerHappy(self,model,fieldname):
-    data = shootsfired.objects.values('unit').annotate(triggerHappy=Count('unit'))
-
-    
+def Extradata(self,model,fieldname,unitName=""):
+    data = model.objects.values(fieldname,unitName).annotate(count=Count(fieldname))
     for id in data:
-        userdata = NewUser.objects.filter(steamID=int(id['unit'])).first()
-        print(userdata.user_name)
+        userdata = NewUser.objects.filter(steamID=int(id[unitName])).first()
         serialDatauser = RegisterNewUser(userdata,many=False)
-        id['unit'] = serialDatauser.data
+        id[unitName] = serialDatauser.data
         
     return data
-
-        
